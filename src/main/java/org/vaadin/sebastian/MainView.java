c0997d961084c566ffcff33b044044380de2ed56
Sebastian Kuehnau <seb@vaadin.com>
2019-07-29 10:27:33 +0200

    clean up (part 2)

--- src/main/java/org/vaadin/sebastian/MainView.java
+++ src/main/java/org/vaadin/sebastian/MainView.java
@@ -1,16 +1,13 @@
 package org.vaadin.sebastian;
 
 import com.vaadin.flow.component.Html;
-import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.data.provider.DataProvider;
 import com.vaadin.flow.data.provider.ListDataProvider;
 import com.vaadin.flow.data.provider.SortDirection;
-import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 import org.vaadin.sebastian.model.Person;
 import org.vaadin.sebastian.model.PersonService;
@@ -19,7 +16,6 @@ import java.util.Comparator;
 import java.util.List;
 
 @Route("")
-@JsModule("./styles.js")
 public class MainView extends VerticalLayout {
 
     final Grid.Column<Person> genderColumn;
